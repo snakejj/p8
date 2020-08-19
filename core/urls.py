d6647f3f8a409b99d785e74ae56d3ffd7754ab90
snakejj <snakejj@github.com>
2020-08-19 09:11:22 +0100

    Setting of the legal notice page

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ app_name = "core"
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('mentions_legales/', views.legal_notice, name='legal_notice'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
 
     return render(request, 'pages/home.html', {'title': "Page d'acceuil",})
+
+
+def legal_notice(request):
+
+    return render(request, 'pages/mentions_legales.html', {'title': "Mentions légales",})
